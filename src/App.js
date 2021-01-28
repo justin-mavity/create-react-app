fef05454fc904b01642e76f1d2bc975987ecb032
justin-mavity <justinmavity1021@gmail.com>
2021-01-28 12:30:29 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,72 +1,126 @@
 import React from "react";
 import axios from "axios";
-import UserCard from "./UserCard";
+import styled from "styled-components";
 
-import "./App.css";
+const StyledApp = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledTitle = styled.h1`
+  font-size: 4rem;
+`;
+
+const StyledForm = styled.form`
+  width: 75%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledInput = styled.input`
+  width: 50%;
+  height: 3vh;
+  font-size: 1.1rem;
+  padding: 0 0.75%;
+  border-radius: 50px;
+  border: 1px solid black;
+  outline: none;
+  margin-bottom: 2%;
+`;
+
+const StyledButton = styled.button`
+  width: 10%;
+  height: 3vh;
+  font-size: 1.1rem;
+  background-color: #36d8ff;
+  border: none;
+  border-radius: 50px;
+  outline: none;
+  margin-bottom: 2%;
+  &:hover {
+    cursor: pointer;
+  }
+  &:active {
+    transform: scale(0.9);
+  }
+`;
+
+const StyledUserCard = styled.div`
+  width: 25%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  padding: 2%;
+`;
+
+const StyledImage = styled.img`
+  border-radius: 50%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+`;
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      data: [],
-      followers: [],
-      username: "justin-mavity",
-    };
-  }
-
-  getUser = (username) => {
-    axios
-      .get(`https://api.github.com/users/${username}`)
-      .then((res) => this.setState({ data: res.data }))
-      .catch((err) => console.log(err));
-  };
-
-  getFollowers = (username) => {
-    axios
-      .get(`https://api.github.com/users/${username}/followers`)
-      .then((res) => this.setState({ followers: res.data }))
-      .catch((err) => console.log(err));
+  state = {
+    users: [],
+    userId: "",
   };
 
   componentDidMount() {
-    this.getUser(this.state.username);
-    this.getFollowers(this.state.followers);
+    this.fetchUsers("djviodes");
   }
 
-  handleChange = (e) => {
-    this.setState({ username: e.target.value });
+  fetchUsers = (user) => {
+    axios
+      .get(`https://api.github.com/users/${user}`)
+      .then((response) => {
+        this.setState({
+          users: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-  submitHandler = (e) => {
-    e.preventDefault();
-    this.getUser(this.state.username);
-    this.setState({
-      username: "",
-    });
+  handleChange = (event) => {
+    this.setState({ userId: event.target.value });
+  };
+
+  handleSearch = (event) => {
+    event.preventDefault();
+    this.fetchUsers(this.state.userId);
+    this.setState({ userId: "" });
   };
 
   render() {
     return (
-      <div className="App">
-        <form onSubmit={this.submitHandler}>
-          <input
+      <StyledApp>
+        <StyledTitle>Search Github Users</StyledTitle>
+        <h2>Stalk Your Github Friends</h2>
+        <StyledForm onSubmit={this.handleSearch}>
+          <StyledInput
+            value={this.state.userId}
+            onChange={this.handleChange}
             type="text"
-            name="username"
-            placeholder="Enter a github username"
-            value={this.state.username}
-            onChange={this.state.handleChange}
+            placeholder="Enter Github Username"
           />
-          <button>Search</button>
-        </form>
-        <br />
-        <div>
-          <h1>This is {this.state.username}'s user card: </h1>
-          <UserCard
-            userInfo={this.state.data}
-            followers={this.state.followers}
+          <StyledButton>Fetch Users</StyledButton>
+        </StyledForm>
+        <StyledUserCard className="userContainer">
+          <p>Username: {this.state.users.login}</p>
+          <p>Name: {this.state.users.name}</p>
+          <p>Public Repositories: {this.state.users.public_repos}</p>
+          <StyledImage
+            width="200"
+            height="200"
+            src={this.state.users.avatar_url}
+            alt={this.state.users.name}
           />
-        </div>
-      </div>
+        </StyledUserCard>
+      </StyledApp>
     );
   }
 }
